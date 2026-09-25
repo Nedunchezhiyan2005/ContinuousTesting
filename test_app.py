1c7050f0caea01147d8fc4f4f47d9d80156d3a49
Nedunchezhiyan_A <nedunchezhiyan532@gmail.com>
2026-09-25 09:40:47 +0530

    Add continuous testing Jenkins pipeline

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app import add, multiply
+
+class TestApp(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(4, 5), 20)
+
+if __name__ == "__main__":
+    unittest.main()
